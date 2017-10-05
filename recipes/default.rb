9623151124f05c3c655a4316bc06d11d87c23b39
Sean OMeara <sean@sean.io>
2017-10-05 15:57:02 -0400

    adding tests

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,26 +1,26 @@
 
-user "notroot" do
+user 'notroot' do
   action :create
 end
 
-package "netcat" do
+package 'netcat' do
   action :install
 end
 
-template "/etc/systemd/system/echo.service" do
-  source "echo.service.erb"
+template '/etc/systemd/system/echo.service' do
+  source 'echo.service.erb'
   owner 'root'
   group 'root'
   mode '0644'
   variables(
-    description: "an echo server",
+    description: 'an echo server',
     system_user: 'notroot',
-    exec_start: "/bin/netcat -ll -p 9001 -c /bin/cat"
+    exec_start: '/bin/netcat -ll -p 9001 -c /bin/cat'
   )
   action :create
 end
 
-service "echo" do
+service 'echo' do
   provider Chef::Provider::Service::Systemd
   supports status: true
   action [:enable, :start]
---
+++ test/integration/two/assert_functioning_spec.rb
@@ -0,0 +1,9 @@
+describe command('echo "hello" | nc -q 1 one.computers.biz 9001') do
+  its(:exit_status) { should eq 0 }
+  its(:stdout) { should match(/hello/) }
+end
+
+describe command('echo "hello" | nc -q 1 three.computers.biz 9001') do
+  its(:exit_status) { should eq 0 }
+  its(:stdout) { should match(/hello/) }
+end
